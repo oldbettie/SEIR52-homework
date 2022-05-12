181aae9128ad5d7e52f0ddcfc19cd4fc28975ae1
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-05-12 08:33:02 +0800

    updated homework

--- Jayj Eaton/wk10 - starts 9th May/3-wed/search-algorithms/searches.js
+++ Jayj Eaton/wk10 - starts 9th May/3-wed/search-algorithms/searches.js
@@ -1,21 +1,32 @@
-function binarySearch(arr, element) {
-	//  search through the array non-recursively for the element
+function binarySearch(haystack, needle) {
+	let start = 0;
+	let end = haystack.length - 1;
+	let midpoint = Math.floor((start + end) / 2);
 
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === element) {
-			return i;
+	while (haystack[midpoint] !== needle && start < end) {
+		if (needle < haystack[midpoint]) {
+			end = midpoint - 1;
+		} else {
+			start = midpoint + 1;
 		}
+		midpoint = Math.floor((start + end) / 2);
 	}
-	return -1;
-	//  if the element is not found, return -1
-	//  if the element is found, return the index at which it was found
+	return needle === haystack[midpoint] ? midpoint : -1;
 }
 
-function recursiveBinarySearch(arr, element) {
-	//  search through the array recursively for the element
-	//  you may need to add more parameters to this function!
-	//  if the element is not found, return -1
-	//  if the element is found, return the index at which it was found
+function recursiveBinarySearch(
+	haystack,
+	needle,
+	start = 0,
+	end = haystack.length - 1,
+	midpoint = Math.floor((start + end) / 2)
+) {
+	if (haystack[midpoint] === needle) return midpoint;
+	if (start >= end) return -1;
+	if (needle < haystack[midpoint]) {
+		end = midpoint - 1;
+	} else start = midpoint + 1;
+	return recursiveBinarySearch(haystack, needle, start, end);
 }
 
 module.exports = {
